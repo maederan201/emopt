59c5a6870e61f66394ed3fcc8481ad448a6a5cc1
Andreas Maeder <maederan@phys.ethz.ch>
2022-01-31 10:15:14 +0100

    Changed example to trapezoidal ridge waveguide

--- examples/waveguide_modes/wg_modes_3D_LiNbO3.py
+++ examples/waveguide_modes/wg_modes_3D_LiNbO3.py
@@ -12,6 +12,7 @@ If you wish to increase the number of cores that the example is executed on,
 change 8 to the desired number of cores.
 """
 import emopt
+import sys
 from emopt.misc import NOT_PARALLEL
 
 import numpy as np
@@ -21,7 +22,7 @@ from math import pi
 # Set up the size of the problem
 ####################################################################################
 W = 3.0
-H = 2.0
+H = 3.0
 dx = 0.02
 dy = 0.02
 N = int(np.ceil(W/dx)+1)
@@ -34,13 +35,25 @@ wavelength = 1.55
 ####################################################################################
 # Define the material diststriputions!
 ####################################################################################
-eps_LiNbO3_x = 2.14**2
-eps_LiNbO3_y = 2.21**2
-eps_LiNbO3_z = 2.21**2
-eps_SiO2 = 1.45**2
 
-w_wg = 1.75
-h_wg = 0.4
+n_o = 2.211111008653574
+n_e = 2.1375596497855565
+n_SiO2 = 1.444023632787416
+
+eps_LiNbO3_x = n_e**2
+eps_LiNbO3_y = n_o**2
+eps_LiNbO3_z = n_o**2
+eps_SiO2 = n_SiO2**2
+
+w_wg = 1
+h_wg = 0.3
+tf = 0.6
+angle = 65
+h_clad = 1
+
+
+comsol_result = 1.8907191769719351
+
 
 # we need to set up the geometry of the cross section for which the mode will
 # be computed. Here we use emopt.grid objects to define the permittivity and
@@ -48,24 +61,41 @@ h_wg = 0.4
 
 # With the current implementatin of the anisotropic permittivity, we must instatiate three
 # versions of the permittivity: 1 for eps_xx, 1 for eps_yy, and one for eps_zz
-# eps_xx
-strip = emopt.grid.Rectangle(W/2, H/2, w_wg, h_wg)
-bottom_cladding = emopt.grid.Rectangle(W/2, (H/2 - h_wg/2)/2, W, H/2 - h_wg/2)
-eps_bg = emopt.grid.Rectangle(W/2, H/2, 2*W, 2*H)
 
-strip.layer = 1; strip.material_value = eps_LiNbO3_x
+# Define vertices of the polygon
+
+w_wg_bot = w_wg + 2*h_wg/np.tan(angle*np.pi/180.)
+vertices = np.array([
+    [-W/2, tf/2 - h_wg],
+    [-w_wg_bot/2., tf/2 - h_wg],
+    [-w_wg/2., tf/2.],
+    [w_wg/2., tf/2.],
+    [w_wg_bot/2., tf/2. - h_wg],
+    [W/2., tf/2. - h_wg],
+    [W/2., - tf/2.],
+    [-W/2., -tf/2.],
+    [-W/2, tf/2 - h_wg]])
+
+
+# eps_xx
+lnoi = emopt.grid.Polygon(vertices[:,0],vertices[:,1])
+bottom_cladding = emopt.grid.Rectangle(0, -tf/2. - h_clad/2. , W , h_clad)
+eps_bg = emopt.grid.Rectangle(0, tf/2. + (H-h_clad-tf)/2., W, (H-h_clad-tf))
+
+lnoi.layer = 1; lnoi.material_value = eps_LiNbO3_x
 bottom_cladding.layer = 2; bottom_cladding.material_value = eps_SiO2
 eps_bg.layer = 3; eps_bg.material_value = 1.0
 
 eps_x = emopt.grid.StructuredMaterial2D(W,H,dx,dy)
-eps_x.add_primitives([strip, bottom_cladding, eps_bg])
+eps_x.add_primitives([lnoi, bottom_cladding, eps_bg])
+eps_x.add_primitives([lnoi])
 
 # eps_yy
-strip = emopt.grid.Rectangle(W/2, H/2, w_wg, h_wg)
-bottom_cladding = emopt.grid.Rectangle(W/2, (H/2 - h_wg/2)/2, W, H/2 - h_wg/2)
-eps_bg = emopt.grid.Rectangle(W/2, H/2, 2*W, 2*H)
+lnoi = emopt.grid.Polygon(vertices[:,0],vertices[:,1])
+bottom_cladding = emopt.grid.Rectangle(0, -tf/2. - h_clad/2. , W , h_clad)
+eps_bg = emopt.grid.Rectangle(0, tf/2. + (H-h_clad-tf)/2., W, (H-h_clad-tf))
 
-strip.layer = 1; strip.material_value = eps_LiNbO3_y
+lnoi.layer = 1; lnoi.material_value = eps_LiNbO3_y
 bottom_cladding.layer = 2; bottom_cladding.material_value = eps_SiO2
 eps_bg.layer = 3; eps_bg.material_value = 1.0
 
@@ -73,9 +103,9 @@ eps_y = emopt.grid.StructuredMaterial2D(W,H,dx,dy)
 eps_y.add_primitives([strip, bottom_cladding, eps_bg])
 
 # eps_zz
-strip = emopt.grid.Rectangle(W/2, H/2, w_wg, h_wg)
-bottom_cladding = emopt.grid.Rectangle(W/2, (H/2 - h_wg/2)/2, W, H/2 - h_wg/2)
-eps_bg = emopt.grid.Rectangle(W/2, H/2, 2*W, 2*H)
+strip = emopt.grid.Polygon(vertices[:,0],vertices[:,1])
+bottom_cladding = emopt.grid.Rectangle(0, -tf/2. - h_clad/2. , W , h_clad)
+eps_bg = emopt.grid.Rectangle(0, tf/2. + (H-h_clad-tf)/2., W, (H-h_clad-tf))
 
 strip.layer = 1; strip.material_value = eps_LiNbO3_z
 bottom_cladding.layer = 2; bottom_cladding.material_value = eps_SiO2
@@ -88,12 +118,12 @@ eps_z.add_primitives([strip, bottom_cladding, eps_bg])
 eps = [eps_x, eps_y, eps_z]
 
 mu = emopt.grid.ConstantMaterial2D(1.0)
-domain = emopt.misc.DomainCoordinates(0, W, 0, H, 0, 0, dx, dy, 1.0)
+domain = emopt.misc.DomainCoordinates(-W/2., W/2., -tf/2. - h_clad, tf/2. + (H - tf - h_clad), 0, 0, dx, dy, 1.0)
 
 ####################################################################################
 # setup the mode solver
 ####################################################################################
-neigs = 4
+neigs = 1
 modes = emopt.modes.ModeFullVector(wavelength, eps, mu, domain, n0=np.sqrt(eps_LiNbO3_y), neigs=neigs)
 modes.build() # build the eigenvalue problem internally
 modes.solve() # solve for the effective indices and mode profiles
@@ -107,8 +137,8 @@ Ey = modes.get_field_interp(mindex, 'Ey', squeeze=True)
 Ez = modes.get_field_interp(mindex, 'Ez', squeeze=True)
 if(NOT_PARALLEL):
     import matplotlib.pyplot as plt
-
-    print('Effective index = {:.4}'.format(modes.neff[0].real))
+    print('COMSOL Result for Effective index: {:.5}'.format(comsol_result))
+    print('Effective index = {:.5}'.format(modes.neff[0].real))
 
 
     E = np.sqrt(np.abs(Ex)**2 + np.abs(Ey)**2 + np.abs(Ez)**2)
@@ -135,7 +165,7 @@ Ez = modes.get_field_interp(mindex, 'Ez', squeeze=True)
 if(NOT_PARALLEL):
     import matplotlib.pyplot as plt
 
-    print('Effective index = {:.4}'.format(modes.neff[0].real))
+    print('Effective index = {:.5}'.format(modes.neff[0].real))
 
 
     E = np.sqrt(np.abs(Ex)**2 + np.abs(Ey)**2 + np.abs(Ez)**2)
